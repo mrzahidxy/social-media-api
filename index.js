e7baf88eaa2768a1c7a3a317825b58879c5709e1
mrzahidxy <mrzahidxy@gmail.com>
2023-06-13 00:26:01 +0600

    real time message sent & get

--- index.js
+++ index.js
@@ -53,12 +53,25 @@ const removeUser = (socketId) => {
   users = users.filter((user) => user.socketId !== socketId);
 };
 
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 //when connect
 io.on("connection", (socket) => {
   //when disconnect
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
     io.emit("getUsers", users);
+  });
+
+
+  //send & get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
   });
 
   //when disconnect
